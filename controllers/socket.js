b777e5166354a2f5d0c5628dc8e657ec8bae7f60
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-20 08:06:40 -0500

    socket controller

---
+++ controllers/socket.js
@@ -0,0 +1,9 @@
+module.exports = function(io) {
+  io.on('connection', function(socket) {
+    console.log('connected');
+
+    socket.on('disconnect', function() {
+      console.log('disconnected')
+    })
+  })
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const PORT = process.env.PORT || 3001;
+const io = require('socket.io')(server);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./controllers/socket')(io);
+
+server.listen(PORT, () => {
+  console.log(`server is live on http://localhost:${PORT}`)
+})
