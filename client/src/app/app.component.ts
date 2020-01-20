f4ece2cfa065538db78a3fa0f93db1cfb7ea16ad
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-20 10:35:43 -0500

    commented room process

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit, OnDestroy {
   roomPicked: boolean = false;
   rooms: Room[] = [];
   title = 'client';
+  room: string = '';
 
   constructor(public socketService: SocketService) { }
 
@@ -27,15 +28,25 @@ export class AppComponent implements OnInit, OnDestroy {
     this.socketService.socket.emit('hello', { hello: 'world' });
 
     this.socketService.socket.on('room list', (data) => {
-      console.log(data)
+      /* 
+        Gets rooms that are made by socket.io;
+        each id creates a room automatically;
+        maps rooms to create room object which has roomName and members - number of people in the room;
+        then filters out rooms with player- in the name, which are rooms created by players;
+        then slices out 'player-' from room name for display to page;
+      */
+
       const roomsArray: string[] = Object.keys(data);
-      console.log(roomsArray)
       const newArr: Room[] = roomsArray.map(room => {
         return { roomName: room, members: data[room].length }
       })
-      console.log(newArr);
+
       const playerRoomArr: Room[] = newArr.filter(room => room.roomName.includes('player-'));
-      console.log(playerRoomArr);
+      playerRoomArr.forEach(room => {
+        room.roomName = room.roomName.slice(7)
+      })
+
+      console.log(playerRoomArr)
       this.rooms = playerRoomArr;
     })
   }
@@ -46,5 +57,17 @@ export class AppComponent implements OnInit, OnDestroy {
 
   testRoomJoin() {
     this.socketService.socket.emit('join room', { room: 'Test1' })
+    this.room = 'Test1';
+  }
+
+  testRoomClick(players, roomName) {
+    console.log(players) // I only want to be able to join a room if there is 1 person in the members list
+    if (players === 1) {
+      console.log('would join room');
+      this.socketService.socket.emit('join room', { room: roomName })
+      this.room = roomName;
+    } else {
+      console.log('would not join room');
+    }
   }
 }
